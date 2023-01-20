975a267098deefbb378545b1a744318618ca26fa
samim.azimi <samimelite8@gmail.com>
2023-01-20 14:23:32 +0430

    sunil phone added

--- src/components/Contactus.js
+++ src/components/Contactus.js
@@ -10,7 +10,7 @@ import { BsPerson } from 'react-icons/bs';
 import { GrSend } from 'react-icons/gr';
 
 
-import { toast} from "react-toastify";
+import { toast } from "react-toastify";
 import emailjs from "emailjs-com";
 
 // import ReactMapGL from "react-map-gl";
@@ -72,9 +72,15 @@ function Contactus() {
                 <div className='details'>
                     <div className='detailsList'>
                         <ul className='contactDetailsList'>
-                            <li className='contactDetailsListItems'><img src={ContactIcons} alt="footerIcons" className='footerIcons1Section' /><p className='phonenumber'>+7 707 647 2852</p></li>
+                            <li className='contactDetailsListItems'><img src={ContactIcons} alt="footerIcons" className='footerIcons1Section' />
+
+                                <div className='phoneGroups'>
+                                    <p className='phonenumber'>+7 707 647 2852</p>
+                                    <p className='phonenumber'>+7 707 898 0363</p>
+                                </div>
+                            </li>
                             <li className='contactDetailsListItems'><img src={Gmail} alt="footerIcons" className='footerIcons1Section' /><a className='mailto' href="mailto:sanroom@gmail.com">sanroom.sales@gmail.com</a></li>
-                            <li className='contactDetailsListItems'><img src={LocationIcon} alt="footerIcons" className='footerIcons1Section' /><p className='address'>Chimkent, Kazakistan</p></li>
+                            <li className='contactDetailsListItems'><img src={LocationIcon} alt="footerIcons" className='footerIcons1Section' /><p className='address'>Almaty, Kazakistan</p></li>
                         </ul>
                     </div>
                     <div className='socialMediaContactPage'>
@@ -124,7 +130,7 @@ function Contactus() {
                 </iframe>
 
             </div>
-            
+
 
         </>
     )
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,12 +12,16 @@ function Footer() {
         <footer>
             <section className='contactDetailsFooter'>
                 <ul className='contactDetailsList'>
-                    <li className='contactDetailsListItems'><img src={ContactIcons} alt="footerIcons" className='footerIcons1Section' /><p className='phonenumber'>+77076472852</p></li>
+                    <li className='contactDetailsListItems'><img src={ContactIcons} alt="footerIcons" className='footerIcons1Section' />
+                        <div className='phoneGroupsFooter'>
+                            <p className='phonenumber'>+7 707 647 2852</p>
+                            <p className='phonenumber'>+7 707 898 0363</p>
+                        </div>
+                    </li>
                     <li className='contactDetailsListItems'><img src={Gmail} alt="footerIcons" className='footerIcons1Section' /><a className='mailto' href="mailto:sanroom@gmail.com">sanroom.sales@gmail.com</a></li>
-                    <li className='contactDetailsListItems'><img src={LocationIcon} alt="footerIcons" className='footerIcons1Section' /><p className='address'>Çımkent, Kazakhstan</p></li>
+                    <li className='contactDetailsListItems'><img src={LocationIcon} alt="footerIcons" className='footerIcons1Section' /><p className='address'>Almaty, Kazakhstan</p></li>
                 </ul>
             </section>
-
             <section className='socialMediaIcons'>
                 <img src={Fb} alt="Facebook Icon" className="facebook" />
                 <img src={Yt} alt="Youtube Icon" className="youtube" />
@@ -26,7 +30,6 @@ function Footer() {
             <section className='logoFooter'>
                 <img src={SanroomLogo} alt="SANROOM Logo" />
             </section>
-
         </footer>
     )
 }
